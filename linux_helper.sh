ebdf4104803a2f612b798d6a46de6b6912706b69
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-06-15 14:12:35 +0200

    Linux helper

---
+++ linux_helper.sh
@@ -0,0 +1,48 @@
+sudo apt update
+sudo apt upgrade
+
+sudo apt install git -y
+sudo apt install build-essential
+sudo apt install cmake
+sudo apt install mpv
+sudo apt install ffmpeg
+sudo apt install curl
+
+##tkinter
+sudo apt install python3-tk
+
+
+##Change grub stuff
+sudo nano /etc/default/grub
+sudo update-grub
+
+##Install brave
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+sudo apt update
+sudo apt install brave-browser
+
+##VSCode
+https://code.visualstudio.com/docs/setup/linux
+
+##Install github desktop mwt >>
+https://github.com/shiftkey/desktop?tab=readme-ov-file#installation-via-package-manager
+
+##Aliasing
+echo "alias die='shutdown now'" >> ~/.bashrc
+echo "alias rbt='reboot'" >> ~/.bashrc
+echo "alias brv='brave browser'" >> ~/.bashrc
+echo "alias ghub='github-desktop'" >> ~/.bashrc
+source ~/.bashrc
+
+
+cd "$HOME"
+mkdir scripts
+mkdir gitclones
+cd gitclones
+
+git clone https://github.com/MisterChamster/private-image-to-audio-embedder.git img-to-audio
+git clone https://github.com/MisterChamster/private-image-worker.git img-worker
+git clone https://github.com/MisterChamster/private-yt-downloader.git yt-download
+git clone https://github.com/MisterChamster/private-audio-metadata-worker.git audio-mtd-worker
+
