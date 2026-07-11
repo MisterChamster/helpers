aab524eb8ba628e1e1367a599609bc0937529c9a
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-07-11 13:02:32 +0200

    New Path methods

--- python_pathlib_helper.py
+++ python_pathlib_helper.py
@@ -41,6 +41,8 @@ print(my_path.iterdir()) #Only on dir! Can iterate over to fill list
 print(Path.cwd())     #cwd
 print(Path.home())    #User home dir
 print(my_path.home()) #Return home path (Path('home/user'))
+print(my_path.expanduser()) #Replace ~ with home dir
+print(my_path.resolve())    #Print full path
 
 #Ints
 #Creates if doesn't exist and writes (not appends!). Only files
@@ -69,12 +71,33 @@ my_path.replace(newpath)
            # if it already exists (subject to OS restrictions,
            # e.g., replacing a non-empty directory).
 
+my_path.chmod() # For files and dirs!
+                # 777 Read, write, execute for everyone
+                # 444 Read-only for everyone
+                # 755 Owner: rwx, Group: rx, Others: rx
+                # 700 Owner: rwx only
+                # 644 Owner: rw, Group: r, Others: r
+                # 600 Owner: rw only
+mode = oct(my_path.stat().st_mode)[-3:] #Check perms
+
+my_path.touch() #Creates file
+                #mode=777        perms
+                #exist_ok=True   doesn't raise error if already exists
+
+my_path.mkdir() #Creates directory
+                #mode=777        perms
+                #parents=True    creates parent (nested) dirs if true
+                #exist_ok=True   doesn't raise error if already exists
+
+my_path.write_text("Helloooo")
+        # Creates the file if it doesn't exist.
+        # Overwrites the file if it already exists.
+        # Returns the number of characters written.
+my_path.write_bytes(b"\x41\x42\x43")     #ABC
+my_path.write_bytes(bytes([41, 42, 43])) #ABC
+
 my_path.unlink(missing_ok=True)  #Remove file or link
 Path('path/to/emptydir').rmdir() #Remove empty directory
 
 import shutil
 shutil.rmtree(Path("non/empty/dir")) #Remove non empty directory
-
-
-
-# chmod, expanduser, home, mkdir, resolve, touch, write_bytes, write_text
