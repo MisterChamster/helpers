56d64c0d283eeb87dba1c4d369136c97476f520d
Mr Chamster <s.duda03@gmail.com>
2026-07-17 00:59:33 +0200

    Filtering/mapping

--- python_lists_algorithms_helper.py
+++ python_lists_algorithms_helper.py
@@ -85,43 +85,50 @@ if True:
 
 
 # Del duplicates
-# if True:
-numbers = [4, 2, 4, 5, 2, 8, 5]
-result = []
+if True:
+    numbers = [4, 2, 4, 5, 2, 8, 5]
+    result = []
 
-sorted_numbers = [2, 2, 4, 4, 4, 5, 8]
-sorted_result = [numbers[0]]
-write = 1
+    sorted_numbers = [2, 2, 4, 4, 4, 5, 8]
+    sorted_result = [numbers[0]]
+    write = 1
 
-consecutive_numbers = [1, 1, 2, 2, 3, 1, 1]
-consecutive_result = [numbers[0]]
-
-
-for value in numbers: #preserve order
-    if value not in result:
-        result.append(value)
-
-result = list(set(numbers))  #no order
-
-for value in sorted_numbers[1:]:    #for sorted, order
-    if value != sorted_result[-1]:
-        sorted_result.append(value)
-
-for value in consecutive_numbers[1:]:  # for consecutive
-    if value != consecutive_result[-1]:
-        consecutive_result.append(value)
-
-for read in range(1, len(sorted_numbers)):   # sorted, in place
-    if sorted_numbers[read] != sorted_numbers[write - 1]:
-        sorted_numbers[write] = sorted_numbers[read]
-        write += 1
-del sorted_numbers[write:]
+    consecutive_numbers = [1, 1, 2, 2, 3, 1, 1]
+    consecutive_result = [numbers[0]]
 
 
+    for value in numbers: #preserve order
+        if value not in result:
+            result.append(value)
 
+    result = list(set(numbers))  #no order
+
+    for value in sorted_numbers[1:]:    #for sorted, order
+        if value != sorted_result[-1]:
+            sorted_result.append(value)
+
+    for value in consecutive_numbers[1:]:  # for consecutive
+        if value != consecutive_result[-1]:
+            consecutive_result.append(value)
+
+    for read in range(1, len(sorted_numbers)):   # sorted, in place
+        if sorted_numbers[read] != sorted_numbers[write - 1]:
+            sorted_numbers[write] = sorted_numbers[read]
+            write += 1
+    del sorted_numbers[write:]
 
 
 # Sort
 
+
+
+
 # Filtering/mapping
+if True:
+    list(map(lambda x: x * 2, [1, 2, 3]))            # Mutate elements
+                                                     # [2, 4, 6]
+    list(filter(lambda x: x % 2 == 0, [1, 2, 3, 4])) # Remove elements
+                                                     # [2, 4]
+
+
 # Aggregation (sum, avg, median, product)
