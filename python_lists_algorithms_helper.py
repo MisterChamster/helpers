0b2e6ce526fc061e15129aa6f9b52ccab18943e5
Mr Chamster <s.duda03@gmail.com>
2026-07-16 22:22:48 +0200

    Search sorted

--- python_lists_algorithms_helper.py
+++ python_lists_algorithms_helper.py
@@ -30,6 +30,26 @@ if True:
 
 
 
+# Search
+if True:
+    numbers = [1, 2, 4, 5, 7, 8, 10, 15]
+    target = 8
+    left = 0
+    right = len(numbers) - 1
+
+    while left <= right:     # one occurence, for SORTED!
+        middle = (left + right) // 2
+
+        if numbers[middle] == target:
+            print("Found at index", middle)
+            found = True
+            break
+        elif target < numbers[middle]:
+            right = middle - 1
+        else:
+            left = middle + 1
+
+
 
 # Counting
 if True:
