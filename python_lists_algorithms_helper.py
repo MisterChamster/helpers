c89b47bfc6bc78adb7b3fc9e02c4676033ea925b
Mr Chamster <s.duda03@gmail.com>
2026-07-17 00:52:29 +0200

    Min/max, duplicates

--- python_lists_algorithms_helper.py
+++ python_lists_algorithms_helper.py
@@ -1,9 +1,9 @@
-# Traversal
-# Search
+## Traversal
+## Counting
+## Search
+## Min/max
+## Del duplicates
 # Sort
-# Min/max
-# Count
-# Del duplicates
 
 # Filtering/mapping
 # Aggregation (sum, avg, median, product)
@@ -29,6 +29,27 @@ if True:
         print(name, num)
 
 
+# Counting
+if True:
+    numbers = [4, 2, 4, 5, 4, 8]
+    target = 4
+    count = 0
+    frequency = {}
+
+
+    for _ in numbers: # Count el
+        count += 1
+
+    for value in numbers: # Count conditional
+        if value == target:
+            count += 1
+
+    for value in numbers: # Count all occurances (to dict)
+        if value in frequency:
+            frequency[value] += 1
+        else:
+            frequency[value] = 1
+
 
 # Search
 if True:
@@ -50,24 +71,57 @@ if True:
             left = middle + 1
 
 
-
-# Counting
+# Min/max
 if True:
-    numbers = [4, 2, 4, 5, 4, 8]
-    target = 4
-    count = 0
-    frequency = {}
+    numbers = [12, 5, 18, 3, 9]
+    minimum = numbers[0]
+    maximum = numbers[0]
+
+    for value in numbers[1:]:  # Min and max, single traversal
+        if value < minimum:
+            minimum = value
+        if value > maximum:
+            maximum = value
 
 
-    for _ in numbers: # Count el
-        count += 1
+# Del duplicates
+# if True:
+numbers = [4, 2, 4, 5, 2, 8, 5]
+result = []
 
-    for value in numbers: # Count conditional
-        if value == target:
-            count += 1
+sorted_numbers = [2, 2, 4, 4, 4, 5, 8]
+sorted_result = [numbers[0]]
+write = 1
 
-    for value in numbers: # Count all occurances (to dict)
-        if value in frequency:
-            frequency[value] += 1
-        else:
-            frequency[value] = 1
+consecutive_numbers = [1, 1, 2, 2, 3, 1, 1]
+consecutive_result = [numbers[0]]
+
+
+for value in numbers: #preserve order
+    if value not in result:
+        result.append(value)
+
+result = list(set(numbers))  #no order
+
+for value in sorted_numbers[1:]:    #for sorted, order
+    if value != sorted_result[-1]:
+        sorted_result.append(value)
+
+for value in consecutive_numbers[1:]:  # for consecutive
+    if value != consecutive_result[-1]:
+        consecutive_result.append(value)
+
+for read in range(1, len(sorted_numbers)):   # sorted, in place
+    if sorted_numbers[read] != sorted_numbers[write - 1]:
+        sorted_numbers[write] = sorted_numbers[read]
+        write += 1
+del sorted_numbers[write:]
+
+
+
+
+
+# Sort
+
+# Filtering/mapping
+# Aggregation (sum, avg, median, product)
